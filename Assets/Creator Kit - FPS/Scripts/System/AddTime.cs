acd7a4822e9e0c4e5fb468c26b5524f0a2deeb6e
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2023-06-17 23:45:26 +0800

    :sparkle: Added Time component script

---
+++ Assets/Creator Kit - FPS/Scripts/System/AddTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class AddTime : MonoBehaviour
+{
+    public float addToTime;
+    
+    void Reset()
+    {
+        gameObject.layer = LayerMask.NameToLayer("PlayerCollisionOnly");
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Controller c = other.GetComponent<Controller>();
+
+        if (c != null)
+        {
+            GameSystem.Instance.AddTimer(addToTime);
+            Destroy(gameObject);
+        }
+    }
+}
